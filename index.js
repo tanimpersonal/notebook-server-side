bc79a4e230d5b89950ce5f907c9a1c83c4d5efa0
tanimpersonal <tashintanim2019@gmail.com>
2022-08-11 05:21:28 +0600

    delete added

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ async function run() {
       const result = await notesCollection.updateOne(filter, updateNote);
       res.send(result);
     });
+    app.delete("/notes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await notesCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
