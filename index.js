f508e04338b3f3862028a267bf0d5267f2d480cd
tanimpersonal <tashintanim2019@gmail.com>
2022-08-11 01:25:24 +0600

    mongo added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -5,22 +6,29 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 app.use(express.json());
-require("dotenv").config();
-
+console.log(process.env);
 //mongo
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.755op.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.755op.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const notesCollection = client.db("notebook").collection("notes");
+    app.get("/notes", async (req, res) => {
+      const query = {};
+      const cursor = notesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
