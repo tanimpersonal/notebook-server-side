64ecb4f9f82dc71ce0da66151a634c643c1c5eab
tanimpersonal <tashintanim2019@gmail.com>
2022-08-11 00:58:01 +0600

    mongo initial

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+//mongo
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.755op.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("connected");
+  // perform actions on the collection object
+  client.close();
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
