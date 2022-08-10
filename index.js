d549e1809cc0418e66ad7d17be45f97e92bb1704
tanimpersonal <tashintanim2019@gmail.com>
2022-08-11 04:14:12 +0600

    update

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
-console.log(process.env);
+
 //mongo
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.755op.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +23,21 @@ async function run() {
       const query = {};
       const cursor = notesCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.put("/notes/:id", async (req, res) => {
+      console.log(req.body);
+      const id = req.params.id;
+      const body = req.body.initialValue;
+      const filter = { _id: ObjectId(id) };
+      console.log(body);
+      const updateNote = {
+        $set: {
+          title: body.title,
+          body: body.body,
+        },
+      };
+      const result = await notesCollection.updateOne(filter, updateNote);
       res.send(result);
     });
   } finally {
